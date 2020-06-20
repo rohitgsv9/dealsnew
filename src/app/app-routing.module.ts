238e13aa14009942cd19b8e5d45361144e0fb489
rohitgsv9 <rohitgsv9@gmail.com>
2020-06-20 10:12:58 +0530

    FA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { ProductComponent} from './product/product.component';
 import { CardComponent } from './card/card.component';
 import { CardsComponent } from './cards/cards.component';
 import { AppComponent } from './app.component';
-
+import { FlipkartComponent } from './flipkart/flipkart.component';
+import { AmazonComponent} from './amazon/amazon.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AdminIndexComponent } from './admin_area/admin-index/admin-index.component';
 import { AddBlogComponent } from './admin_area/add-blog/add-blog.component';
@@ -17,6 +18,7 @@ import { AdminLoginComponent } from './admin_area/admin-login/admin-login.compon
 import { AddDealByLinkComponent } from './admin_area/add-deal-by-link/add-deal-by-link.component';
 import { AddCaroComponent } from './admin_area/add-caro/add-caro.component';
 import { UpdateCaroComponent } from './admin_area/update-caro/update-caro.component';
+import { from } from 'rxjs';
 
 
 
@@ -27,29 +29,19 @@ const routes: Routes = [
   {path:'app', component:AppComponent},
   {path:'cards', component:CardsComponent},
   {path:'card', component:CardComponent},
+  {path:'amazon', component:AmazonComponent},
+  {path:'flipkart', component:FlipkartComponent},
+  {path:'admin', component:AdminIndexComponent},
+  {path:'addBlog', component:AddBlogComponent},
+  {path:'addDeal', component:AddDealComponent},
+  {path:'updateBlog', component:UpdateBlogComponent},
+  {path:'updateDeal', component:UpdateDealComponent},
   {path:'blog', component:BlogComponent},
   {path:'blogview', component:BlogViewComponent},
-
-  { 
-    path:'login',
-    component:AdminLoginComponent,
-    children:[
-      {
-      
-        path:'admin',
-        component:AdminIndexComponent,
-        children: [
-          {path:'addBlog', component:AddBlogComponent},
-      {path:'addDeal', component:AddDealComponent},
-      {path:'updateBlog', component:UpdateBlogComponent},
-      {path:'updateDeal', component:UpdateDealComponent},
-      {path:'byLink', component:AddDealByLinkComponent},
-      {path:'addCaro', component:AddCaroComponent},
-      {path:'updateCaro', component:UpdateCaroComponent}
-         ],
-      }
-    ]
-  },
+  {path:'login', component:AdminLoginComponent},
+  {path:'byLink', component:AddDealByLinkComponent},
+  {path:'addCaro', component:AddCaroComponent},
+  {path:'updateCaro', component:UpdateCaroComponent},
   {path:'**', component:PageNotFoundComponent}
 ]; 
 
